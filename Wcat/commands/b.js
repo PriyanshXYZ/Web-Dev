016891b69946f53677c3d88cb5a82ab3cb3194d0
PriyanshXYZ <priyanshragit@gmail.com>
2021-12-13 14:52:41 +0530

    changes in code

--- Wcat/commands/b.js
+++ Wcat/commands/b.js
@@ -1,4 +1,14 @@
 function BOption(contentArr){
+    for (let i = 1; i < contentArr.length; i++) {
+        if (contentArr[i] == "" && contentArr[i - 1] != "")
+            contentArr[i] = null;
+        else if (contentArr[i] == "" && contentArr[i - 1] == null)
+            contentArr[i] = null;
+    }
+    let tempArr = [];
+    for (let i = 0; i < contentArr.length; i++)
+        if (contentArr[i] != null) tempArr.push(contentArr[i])
+    contentArr = tempArr;
 
     let counter=1
     for(let i = 0; i <contentArr.length; i++){
